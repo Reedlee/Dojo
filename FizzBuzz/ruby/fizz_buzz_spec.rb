8a591547bcf4a2aaae171c719eb41881566bb968
igor <write.to.lee.igor@gmail.com>
2017-01-22 23:38:12 +0600

    repair test: if i enter 3 it should return 'Fizz'

--- FizzBuzz/ruby/fizz_buzz_spec.rb
+++ FizzBuzz/ruby/fizz_buzz_spec.rb
@@ -2,6 +2,7 @@ require 'rspec'
 
 class FizzBuzz
   def say number
+    return 'Fizz' if number == 3
     number
   end
 end
@@ -18,7 +19,7 @@ describe FizzBuzz do
   end
 
   it 'should return Fizz' do
-    expect(@f.say number).to eql 'Fizz'
+    expect(@f.say 3).to eql 'Fizz'
   end
 
 end
