a267a13565cc6869d6fcfbdee204e8fdc72c77ba
igor <write.to.lee.igor@gmail.com>
2017-01-29 00:40:49 +0600

    add test: 10 should be equal X

--- ruby/roman_number_spec.rb
+++ ruby/roman_number_spec.rb
@@ -27,7 +27,8 @@ describe RomanNumber do
       6 => 'VI',
       7 => 'VII',
       8 => 'VIII',
-      9 => 'IX'
+      9 => 'IX',
+      10 => 'X'
   }.each do |normal_number, roman_number|
     it "#{normal_number} should be equal #{roman_number}" do
       expect(@r.to_roman normal_number).to eql roman_number
