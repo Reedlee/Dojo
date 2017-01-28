b42f9d840999897179a550869ed839f0fa740bf3
igor <write.to.lee.igor@gmail.com>
2017-01-29 00:34:57 +0600

    rewrote methods to find common logic

--- ruby/roman_number_spec.rb
+++ ruby/roman_number_spec.rb
@@ -5,11 +5,11 @@ class RomanNumber
     return 'I' if number == 1
     return 'II' if number == 2
     return 'III' if number == 3
-    return 'IV' if number == 4
+    return 'IV' if number == 5 - 1
     return 'V' if number == 5
-    return 'VI' if number == 6
-    return 'VII' if number == 7
-    return 'VIII' if number == 8
+    return 'VI' if number == 5 + 1
+    return 'VII' if number == 5 + 2
+    return 'VIII' if number == 5 + 3
     return 'IX' if number == 9
   end
 end
