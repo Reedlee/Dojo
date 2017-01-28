27e259f77f2b6d8313c7fd06fe0d34ffb08a223f
igor <write.to.lee.igor@gmail.com>
2017-01-29 00:11:49 +0600

    add test:  2 should be equal II

--- ruby/roman_number_spec.rb
+++ ruby/roman_number_spec.rb
@@ -11,5 +11,10 @@ describe RomanNumber do
   it '1 should be equal I' do
     r = RomanNumber.new
     expect(r.to_roman 1).to eql 'I'
+    end
+
+  it '2 should be equal II' do
+    r = RomanNumber.new
+    expect(r.to_roman 2).to eql 'II'
   end
 end
