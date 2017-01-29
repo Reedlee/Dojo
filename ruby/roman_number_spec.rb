8899a8b542422af3f3649cb1466f74293d401c53
igor <write.to.lee.igor@gmail.com>
2017-01-29 12:58:21 +0600

    added test: 50 equal L

--- ruby/roman_number_spec.rb
+++ ruby/roman_number_spec.rb
@@ -38,6 +38,7 @@ describe RomanNumber do
       15 => 'XV',
       19 => 'XIX',
       20 => 'XX',
+      50 => 'L'
   }.each do |normal_number, roman_number|
     it "#{normal_number} should be equal #{roman_number}" do
       expect(@r.to_roman normal_number).to eql roman_number
