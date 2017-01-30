5c7f0e666e0fbba3bd942c22714ebe8ea0f9ff25
igor <write.to.lee.igor@gmail.com>
2017-01-31 00:19:41 +0600

    added test: 90 should be equal XC

--- ruby/roman_number_spec.rb
+++ ruby/roman_number_spec.rb
@@ -52,6 +52,7 @@ describe RomanNumber do
       44 => 'XLIV',
       100 => 'C',
       101 => 'CI',
+      90 => 'XC',
   }.each do |normal_number, roman_number|
     it "#{normal_number} should be equal #{roman_number}" do
       expect(@r.to_roman normal_number).to eql roman_number
