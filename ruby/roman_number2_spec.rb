acde533f6ad637722d7931444ba886bc14d879d6
igor <write.to.lee.igor@gmail.com>
2017-02-08 21:44:58 +0600

    repaired test: II should be equal 2

--- ruby/roman_number2_spec.rb
+++ ruby/roman_number2_spec.rb
@@ -1,7 +1,8 @@
 class RomanNumber
 
   def to_normal roman_number
-    1 if roman_number == 'I'
+    return 1 if roman_number == 'I'
+    return 2 if roman_number == 'II'
   end
 end
 
@@ -13,7 +14,7 @@ describe RomanNumber do
     expect(r.to_normal 'I').to eql 1
   end
 
-  it 'should I should be equal 1' do
+  it 'should II should be equal 1' do
     r = RomanNumber.new
     expect(r.to_normal 'II').to eql 2
   end
