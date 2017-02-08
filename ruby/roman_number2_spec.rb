6fad9d470f703ddb20105aee49359ab4261d2288
igor <write.to.lee.igor@gmail.com>
2017-02-08 21:34:45 +0600

    added test: I should be equal 1

--- ruby/roman_number2_spec.rb
+++ ruby/roman_number2_spec.rb
@@ -1,8 +1,12 @@
+class RomanNumber
+
+end
+
 
 describe RomanNumber do
 
   it 'should I should be equal 1' do
-
-    true.should == false
+    r = RomanNumber.new
+    expect(r.to_normal 'I').to eql 1
   end
 end
